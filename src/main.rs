1312f1c447671b767f9157a03a3f1f77d3f09441
yedpodtrzitko <yed@vanyli.net>
2023-03-24 15:26:58 +0700

    misc: use if let

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-use std::{env};
+use chrono::Local;
+use git_info;
+use nu_ansi_term::Color;
 use std::convert::From;
+use std::env;
 use std::iter::Iterator;
-use std::option::Option::{None, Some};
+use std::option::Option::Some;
 use std::result::Result::{Err, Ok};
 use std::string::{String, ToString};
 use std::vec::Vec;
-use git_info;
-use chrono::{Local};
-use nu_ansi_term::{Color};
 
 fn main() {
     let mut cwd = match env::current_dir() {
-        Ok(dir) => {
-            dir.display().to_string()
-        }
+        Ok(dir) => dir.display().to_string(),
         Err(e) => {
             println!("{}", e);
             String::from("unknown dir")
@@ -24,12 +22,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let ret_code: i16 = args.get(1).unwrap_or(&String::from("0")).parse().unwrap();
     let color_time = match ret_code {
-        0 => {
-            Color::LightGreen
-        }
-        1..=254 => {
-            Color::Red
-        }
+        0 => Color::LightGreen,
+        1..=254 => Color::Red,
         err => {
             println!("ret code not match {}", err);
             Color::Red
@@ -39,44 +33,35 @@ fn main() {
     // working directory
     let home_prefix = env::var("HOME").unwrap_or(String::from("no home dir"));
     if cwd.starts_with(&home_prefix) {
-        cwd = format!("~{}", cwd.strip_prefix(&home_prefix).unwrap_or("-invalid cwd-"));
+        cwd = format!(
+            "~{}",
+            cwd.strip_prefix(&home_prefix).unwrap_or("-invalid cwd-")
+        );
     }
 
     let mut prompt_items: Vec<String> = vec![];
-    let current_time = Local::now().format("%H:%M:%S");
-    prompt_items.push(color_time.bold().paint(current_time.to_string()).to_string());
+    let current_time = Local::now().format("%H:%M:%S").to_string();
+    prompt_items.push(color_time.bold().paint(current_time).to_string());
     prompt_items.push(Color::Cyan.bold().paint(cwd).to_string());
 
-    match env::var("VIRTUAL_ENV") {
-        Ok(v) => {
-            let venv_splat: Vec<&str> = v.split('/').collect();
-            let venv_name = *(venv_splat.last().unwrap());
-            let emoji_snake = '\u{1F40D}';
-            //let venv_output = format!(" {} {} ", emoji_snake, venv_name);
-            //prompt_items.push(String::from(""));
-            prompt_items.push(Color::Yellow.paint(emoji_snake.to_string()).to_string());
-            prompt_items.push(Color::Yellow.bold().paint(venv_name).to_string());
-        }
-        Err(_) => {}
+    if let Ok(venv) = env::var("VIRTUAL_ENV") {
+        let venv_splat: Vec<&str> = venv.split('/').collect();
+        let venv_name = *(venv_splat.last().unwrap());
+        let emoji_snake = '\u{1F40D}';
+        prompt_items.push(Color::Yellow.paint(emoji_snake.to_string()).to_string());
+        prompt_items.push(Color::Yellow.bold().paint(venv_name).to_string());
     }
 
-    let info = git_info::get();
-    match info.current_branch {
-        Some(branch) => {
-            let emoji_git = '\u{1F517}';
-            /*
-            let emoji_dirty = if info.dirty.unwrap() {
-                '\u{1F7E2}'
-            } else {
-                '\u{274C}'
-            };*/
-            //let output_git = format!(" {} {} ", emoji_git, branch);
-            //let output_git_color = Color::White.bold().paint(output_git).to_string();
-            //prompt_items.push(String::from(""));
-            prompt_items.push(emoji_git.to_string());
-            prompt_items.push(Color::White.bold().paint(branch).to_string());
-        }
-        None => {}
+    if let Some(git_branch) = git_info::get().current_branch {
+        /*
+        let emoji_dirty = if info.dirty.unwrap() {
+            '\u{1F7E2}'
+        } else {
+            '\u{274C}'
+        };*/
+        let emoji_git = '\u{1F517}';
+        prompt_items.push(emoji_git.to_string());
+        prompt_items.push(Color::White.bold().paint(git_branch).to_string());
     }
 
     println!("{}\n$", prompt_items.join(" "));
